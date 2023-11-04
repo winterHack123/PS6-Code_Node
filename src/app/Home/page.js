13e064bb0bc037bbebadbc2e87de1848413f5766
Saksham jain <130843255+Jsakshxm@users.noreply.github.com>
2023-11-04 14:49:16 +0530

    added home page

---
+++ src/app/Home/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link"
+const page = () => {
+  return (
+    <div><Link href="/Home/Signup"><button> Sign Up</button></Link><br />
+    <button>Login</button></div>
+  )
+}
+
+export default page
